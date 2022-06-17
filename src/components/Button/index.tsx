1014062b17cc996fe4d587d411f02ad186d4875b
graziellecafe <65823579+graziellecafe@users.noreply.github.com>
2022-06-17 11:37:30 -0300

    Criação dos componentes

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Buttom extends React.Component {
+    render() {
+        return(
+            <button>Botão</button>
+        )
+    }
+}
+
+export default Buttom;
